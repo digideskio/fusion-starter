e72172299de994447ff34da25746ecbaa56c4bf0
Nathan Trost <Nathaniel.Trost@vinsolutions.com>
2016-09-22 16:54:46 -0500

    refactored code for AppointmentsCard and AppointmentsPage

--- src/components/widgets/AppointmentsCard/AppointmentsPage.js
+++ src/components/widgets/AppointmentsCard/AppointmentsPage.js
@@ -1,52 +1,40 @@
 import React, {Component, PropTypes} from 'react';
-
-
+import {Modal} from 'react-bootstrap';
 // import styles from './dealer.scss';
-
-/*
- AppointmentsPage component.
-    - Shell for other components
-
- Standard Use: <AppointmentsPage id={unique id} appointments={data} />
-
- Requires: React, React-Bootstrap, React-Entypo
-
- Parameters:
-   id: string
-      -unique id related to component, Required
-
-   appointments: formatted Array
-      - An array of values pertaining to an individual customer's appointments, Required
-      [
-       {
-       id: number
-          - unique id of appointment
-
-       time: string
-
-       type: string,
-       status: string,
-       vehicle: object
-         {
-           id: number,
-           year: number,
-           make: string,
-           model: string
-       }
-     }]
-
- */
 
 class AppointmentsPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showAppointmentsModal: false
+    };
+  }
+
+  toggleAppointmentsModal(){
+    this.setState({showAppointmentsModal: !this.state.showAppointmentsModal});
   }
 
   render() {
+    const {
+      appointments,
+      ...other
+    } = this.props;
+
     return(
-      <div>
-        Appointments Page Content
-      </div>
+      <Modal dialogClassName="modal-full" className="appointmentscard__fullpage" {...other}
+             show={this.state.showAppointmentsModal}
+             onHide={this.toggleAppointmentsModal}
+             container={this}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="display-4">Kyla Gonzalez</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="container">
+          <div className="appointmentsPage__content">
+            <AppointmentsPage id="appointmentsCard-appointmentsPage" appointments={appointments}/>
+          </div>
+        </Modal.Body>
+      </Modal>
     );
   }
 }
